ca46d550237e029c959ccaac0ed9a3238d8f2dc7
DaniilTMSC12 <cyxho1@gmail.com>
2022-01-17 20:42:39 +0300

    2nd commit

--- Lesson2/src/main/java/Test.java
+++ Lesson2/src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
-    public static void main(String() args) {
-        System.out.println("sdsada");
+    public static void main(String[] args) {
+        System.out.println("sdsadassada");
     }
 }
